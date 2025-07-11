9a18567a9d52b90968a34d633bfd7841f0b9a9b1
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-07-11 05:43:30 -0700

    added resolve section

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, // Warning limit বাড়ানো
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'react-vendor';
-            }
-            if (id.includes('react-router-dom')) {
-              return 'router-vendor';
-            }
-            return 'vendor'; // অন্য সব node_modules আলাদা chunk
-          }
-        }
-      }
-    }
-  }
-});
+})
